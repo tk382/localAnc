7cd97d109d933f83bc897fcfc8026f4f0b03526d
Tae Kim <tk382@uchicago.edu>
2018-01-06 16:34:49 -0600

    replaced random imputation to conditional imputation

--- R/get_target_new.R
+++ R/get_target_new.R
@@ -1,10 +1,16 @@
-get_target_new = function(X, Y, sigmabeta, Sigma, gam, beta, T){
+get_target_new = function(X, YY, sigmabeta, Sigma, gam, beta, T){
   L = 0
+  Y = YY
   for (i in 1:n){
-    ind = which(!is.na(Y[i,]))
-    if(sum(ind)>0){
-      Y[i, -ind] = mvrnormArma(1, rep(0, T-length(ind)), Sigma[-ind,-ind])
+    ind = which(is.na(Y[i,]))
+    if(length(ind)<T){
+      mu1 = rep(0, length(ind))
+      Sigma11 = Sigma[ind,ind]; Sigma12 = Sigma[ind, -ind]; Sigma21 = Sigma[-ind,ind]
+      Sigma22 = Sigma[-ind,-ind]
+      Y[i,ind] = mvrnormArma(1, mu1, Sigma11-Sigma12 %*% solve(Sigma22) %*% Sigma21)
+      # Y[i, -ind] = mvrnormArma(1, rep(0, T-length(ind)), Sigma[-ind,-ind])
     }
+    #print(dmvnrm_arma(Y[i, ], X[i]*beta, Sigma, T, logd=TRUE))
     L = L+dmvnrm_arma(Y[i, ], X[i]*beta, Sigma, T, logd=TRUE)
   }
   ind = which(gam==1);
